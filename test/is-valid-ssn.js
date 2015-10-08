49fab07d610cfeca97b24aeb14c7d43623d6c67a
Francisco Cardoso <francisco@bitreserve.org>
2015-10-08 13:21:52 +0100

    Add maskSNN function

--- test/is-valid-ssn.js
+++ test/is-valid-ssn.js
@@ -1,5 +1,6 @@
 var test = require('tape');
 var isValidSSN = require('../is-valid-ssn');
+var maskSSN = require('../is-valid-ssn').maskSSN;
 
 test('is valid ssn', function (t) {
   t.plan(7);
@@ -11,4 +12,15 @@ test('is valid ssn', function (t) {
   t.equal(isValidSSN('000-11-1111'), false);
   t.equal(isValidSSN({}), false);
   t.equal(isValidSSN(3434), false);
+});
+
+test('mask ssn', function (t) {
+  try {
+    maskSSN('457-55-5462');
+  } catch (e) {
+    t.equal(e.message, 'Invalid Social Security Number');
+    t.end();
+  }
+
+  t.equal(maskSSN('457555462'), 'XXXXX5462');
 });
